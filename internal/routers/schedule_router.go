7e20c2a53732d83f6ac0f201056698cb8e37a73c
JavaKLD <ba.baturin2016@yandex.ru>
2025-04-29 22:02:30 +0200

    логгер для методов http

--- internal/routers/schedule_router.go
+++ internal/routers/schedule_router.go
@@ -2,13 +2,16 @@ package routers
 
 import (
 	"dolittle2/internal/controllers"
+	"dolittle2/internal/middleware"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InitRoutes(e *echo.Echo, controller *controllers.ScheduleController) {
+	e.Use(middleware.ReqLogger)
+
 	e.POST("/schedule", controller.CreateSchedule)
-	e.GET("/schedule", controller.GetUserSchedule)
-	e.GET("/schedule/:schedule_id", controller.GetSchedule)
+	e.GET("/schedules", controller.GetUserSchedule)
+	e.GET("/schedule", controller.GetSchedule)
 	e.GET("/next_takings", controller.GetNextTakings)
 }
